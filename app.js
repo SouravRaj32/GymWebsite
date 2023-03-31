a10665ce5ba18d2259c54e5d304a5d7517225c7d
Sourav <souravraj32@gmail.com>
2023-03-31 06:13:58 +0530

    hey

--- app.js
+++ app.js
@@ -2,48 +2,54 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const mongoose = require('mongoose');
-const port =process.env.PORT || 3000;
-mongoose.connect('mongodb+srv://souravraj32:Kc130deploy@souravbooking.fyxeibc.mongodb.net/dynamic?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('MongoDB connected...'))
-.catch(err => console.log(err));
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3000;
+mongoose
+  .connect(
+    "mongodb+srv://souravraj32:Kc130deploy@souravbooking.fyxeibc.mongodb.net/dynamic?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
 // Define mongoose Schema
 const contactSchema = new mongoose.Schema({
-    name: String,
-    phone: String,
-    email: String,
-    address: String,
-    desc: String
+  name: String,
+  phone: String,
+  email: String,
+  address: String,
+  desc: String,
 });
 
-const Contact = mongoose.model('Contact', contactSchema);
+const Contact = mongoose.model("Contact", contactSchema);
 
 // EXPRESS SPECIFIC STUFF
-app.use('/static', express.static('static')) // For serving static files
-app.use(express.urlencoded())
+app.use("/static", express.static("static")); // For serving static files
+app.use(express.urlencoded());
 
 // PUG SPECIFIC STUFF
-app.set('view engine','pug') // Set the template engine as pug
-app.set('views', path.join(__dirname,'views')) // Set the views directory
- 
-// ENDPOINTS
-app.get('/', (req, res)=>{
-    const con = "This is the best content on the internet so far so use it wisely"
-    const params = {'title': 'PubG is the best game', "content": con}
-    res.status(200).render('index.pug', params);
-})
+app.set("view engine", "pug"); // Set the template engine as pug
+app.set("views", path.join(__dirname, "views")); // Set the views directory
 
-app.post('/', (req, res)=>{
-    const myData = new Contact(req.body);
-    myData.save()
-    .then(()=>{
-        res.send("This item has been saved to the database");
+// ENDPOINTS
+app.get("/", (req, res) => {
+  const con =
+    "This is the best content on the internet so far so use it wisely";
+  const params = { title: "PubG is the best game", content: con };
+  res.status(200).render("index.pug", params);
+});
+
+app.post("/", (req, res) => {
+  const myData = new Contact(req.body);
+  myData
+    .save()
+    .then(() => {
+      res.send("This item has been saved to the database");
     })
-    .catch((err)=>{
-        res.status(400).send("item was not saved to the database");
+    .catch((err) => {
+      res.status(400).send("item was not saved to the database");
     });
-    // res.status(200).render('contact.pug', );
+  // res.status(200).render('contact.pug', );
 });
 // app.post('/', (req, res)=>{
 //     name = req.body.name
@@ -59,8 +65,7 @@ app.post('/', (req, res)=>{
 
 // })
 
-
 // START THE SERVER
-app.listen(port, '0.0.0.0',()=>{
-    console.log(`Server is running at port no ${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running at port no ${port}`);
 });
